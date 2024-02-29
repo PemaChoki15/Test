5e2f82f425a29ee58363d591adae73add1bbcbc0
Pema Choki <02230068.cst@rub.edu.bt>
2024-02-29 12:14:08 +0600

    classwork

---
+++ classwork.py
@@ -0,0 +1,15 @@
+name = input("Enter your name: ")
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Perform arthmetic operations
+addition_result = num1 +num2
+subtraction_result = num1 - num2
+multiplication_result = num1 * num2
+division_result = num1 / num2
+
+print(f"Addition: {addition_result}")
+print(f"Subtraction: {subtraction_result}")
+print(f"Multiplication: {multiplication_result}")
+print(f"Division: {division_result}")
